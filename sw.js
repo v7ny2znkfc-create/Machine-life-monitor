24b55c1d1c448da1257998da01ead754bed72092
v7ny2znkfc-create <v7ny2znkfc@privaterelay.appleid.com>
2026-09-15 13:29:47 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='af-machine-life-v1-5-1';
+const C='af-machine-life-v1-6';
 const A=['./','index.html','manifest.webmanifest','machine-data.json','config.js','icon-192.png','icon-512.png','apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
